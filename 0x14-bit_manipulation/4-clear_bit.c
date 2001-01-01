ca1d190303c80040700229be5d5e86c8d06ad22d
chrizlm <lmchris58@gmail.com>
2001-01-01 03:38:14 +0300

    clear bit at a position

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets a bit i
+n a binary
+ * @n: pointer to number
+ * @index: position
+ *
+ * Return: int
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int count = 0;
+	unsigned long int result;
+	char *_binary;
+
+	_binary = malloc(sizeof(50 * char));
+	if (_binary == NULL)
+		return (-1);
+
+	while (n >= 0)
+	{
+		result = n % 2;
+
+		if (count == index)
+			_binary[count] = '0';
+		else
+		{
+			if (result == 1)
+				_binary[count] = '1';
+			if (result == 0)
+				_binary[count] = '0';
+		};
+
+		n = n / 2;
+		count++;
+	}
+	_binary[count] = '\0';
+
+	n = binary_to_unit(_binary);
+
+	return (1);
+}
