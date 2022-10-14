acc508ba34daaf63357a6f28f4ab0ab5e3c84913
“chrizlm” <lmchris58@gmail.com>
2022-10-14 05:09:30 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+/**
+ * main -Entry point
+ * use of puts function
+ * return 0 as success
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
