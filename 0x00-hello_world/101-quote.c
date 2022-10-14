4f52ed268c37a4704369bf969941b577e8b7d631
“chrizlm” <lmchris58@gmail.com>
2022-10-14 05:56:38 -0700

    printing to std error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ * writing to standard error
+ * Return: 1 as error
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
