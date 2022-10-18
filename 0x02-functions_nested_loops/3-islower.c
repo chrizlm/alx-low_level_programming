48b82c104bb742706f0d64d65d6a4a11eca3cab3
“chrizlm” <lmchris58@gmail.com>
2022-10-18 11:49:33 -0700

    lower case letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * main - entry point
- * and checks the code
- *
- * Return: 0
- */
-
-int main(void)
-{
-	int results;
-
-	results = _islower('H');
-	_putchar(results + '0');
-	results = _islower('h');
-	_putchar(results + '0');
-	_putchar('\n');
-
-	return (0);
-}
-
-/**
  * _islower - function
  * checks whether a letter is
  * lowercase using ascii
