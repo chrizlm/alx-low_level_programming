865d503050a8cb17cf18a26bac8c2797dec341e3
“chrizlm” <lmchris58@gmail.com>
2022-10-18 11:47:32 -0700

    print 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,5 @@
 #include "main.h"
 
-/**
- * main - entry point
- *
- * Return: 0
- */
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
 
 /**
  * print_alphabet_x10 - prints lowercase
