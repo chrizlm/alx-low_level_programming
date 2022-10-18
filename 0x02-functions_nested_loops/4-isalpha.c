ee6e64371ee8728966b2a8eaf617f4e42cf66175
“chrizlm” <lmchris58@gmail.com>
2022-10-18 11:51:13 -0700

    letters only

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * main - entry point
- * test function
- *
- * Return: 0
- */
-
-int main(void)
-{
-	int results;
-
-	results = _isalpha('H');
-	_putchar(results + '0');
-	results = _isalpha('h');
-	_putchar(results + '0');
-	results = _isalpha(';');
-	_putchar(results + '0');
-	_putchar('\n');
-
-	return (0);
-}
-
-/**
  * _isalpha -function
  * checks for letters only
  * @c: ascii value for letter
