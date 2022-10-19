b86787d387c3ed4c2a9dd9218db43a5d6942fb4f
“chrizlm” <lmchris58@gmail.com>
2022-10-18 23:30:04 -0700

    main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,12 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+void print_long(int n)
+{
+	if (n != 0)
+	{
+		print_long(n/10);
+		_putchar((n%10) + '0');
+	}
+}
