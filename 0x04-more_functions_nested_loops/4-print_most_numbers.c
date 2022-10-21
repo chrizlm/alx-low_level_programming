07639460aed78b4d3cb2ea4ade54a17f16773a18
“chrizlm” <lmchris58@gmail.com>
2022-10-21 02:00:41 -0700

    printing most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
