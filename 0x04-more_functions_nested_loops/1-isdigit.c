7172395b36b38b752cddb466f65e0991b38d3caa
“chrizlm” <lmchris58@gmail.com>
2022-10-21 01:36:45 -0700

    checking for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - function
+ * checks for digits
+ * @c: input value
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	int l;
+
+	for (l = 48 ; l <= 57 ; l++)
+	{
+		if (c == l)
+			return (1);
+	}
+	return (0);
+}
