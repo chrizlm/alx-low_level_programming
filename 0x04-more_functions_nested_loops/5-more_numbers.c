e486ffb968c9dc78a46e231ec4dfd1e7e6be1dce
“chrizlm” <lmchris58@gmail.com>
2022-10-21 04:18:13 -0700

    putchar with more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - function
+ * repeats printing numbers
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j, r, n;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			if (j < 10)
+				r = j;
+			else 
+			{
+				r = j / 10;
+				n = j % 10;
+			}
+			_putchar(r + '0');
+			if (j > 9)
+				_putchar(n + '0');
+		}
+		_putchar('\n');
+	}
+}
