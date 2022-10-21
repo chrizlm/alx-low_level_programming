1114f4cc1ae053032399ef0084c373e8a1fb4e77
“chrizlm” <lmchris58@gmail.com>
2022-10-21 01:27:59 -0700

    upper case letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - function
+ * that checks for upper case lettes
+ * @c: input value
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	int l;
+
+	for (l = 65 ; l <= 90 ; l++)
+	{
+		if (c == l)
+			return (1);
+	}
+	return (0);
+}
