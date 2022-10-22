da513ad298b175dedd93ecae71e809c34c38f1e3
“chrizlm” <lmchris58@gmail.com>
2022-10-22 05:43:37 -0700

    print long numbers with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,21 @@ void print_number(int n)
 {
 	int p, q;
 
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
 	p = n % 10;
-	if (n > 9)
+	if (n >= 10)
 	{
 		q = n / 10;
 		print_number(q);
 	}
 	_putchar(p + '0');
+	}
 }
