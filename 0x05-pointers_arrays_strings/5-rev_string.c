c18f87ecd7b627553e24ba84f82515d4892f1690
“chrizlm” <lmchris58@gmail.com>
2022-10-25 13:44:33 -0700

    reversing an array

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - function
+ * @s: input string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int n = 0, count = 0, i, j = 0;
+	char w[10];
+
+	while (s[n] != 0)
+	{
+		count += 1;
+		n++;
+	}
+	for (i = count - 1 ; i >= 0 ; i--)
+	{
+		w[j] = s[i];
+		j++;
+	}
+	for (i = 0 ; i < count ; i++)
+		s[i] = w[i];
+}
