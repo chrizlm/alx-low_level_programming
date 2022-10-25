9fb55443032d50b7422f7a0b0aade168f7f7396a
“chrizlm” <lmchris58@gmail.com>
2022-10-25 13:02:32 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts -function to print string
+ * @str: string input
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int n = 0, count = 0, i;
+	char w;
+
+	while (str[n] != 0)
+	{
+		count += 1;
+		n++;
+	}
+	for (i = 0 ; i <= count ; i++)
+	{
+		w = str[i];
+		_putchar(w);
+	}
+	_putchar('\n');
+}
