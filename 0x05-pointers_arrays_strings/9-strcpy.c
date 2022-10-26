c8f68706e84b9d2fa25818cf2aeac32f4dccd1f2
“chrizlm” <lmchris58@gmail.com>
2022-10-26 04:13:08 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies
+ * @dest: input
+ * @src: input
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, n = 0, count = 1;
+
+	while (src[n] != 0)
+	{
+		count++;
+		n++;
+	}
+	for (i = 0; i < count ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
