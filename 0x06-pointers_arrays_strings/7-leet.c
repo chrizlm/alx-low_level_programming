24a977c2a2270979e7843f8d5ebda67c88cd445a
“chrizlm” <lmchris58@gmail.com>
2022-10-27 06:29:00 -0700

    swap char with numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - function
+ * @a: input
+ * Return: char
+ */
+
+char *leet(char *a)
+{
+	int i, n = 0, count = 0;
+
+	while (a[n] != 0)
+	{
+		count++;
+		n++;
+	}
+	for (i = 0 ; i < count ; i++)
+	{
+		if (a[i] == 'a' || a[i] == 'A')
+			a[i] = '4';
+		if (a[i] == 'e' || a[i] == 'E')
+			a[i] = '3';
+		if (a[i] == 'o' || a[i] == 'O')
+			a[i] = '0';
+		if (a[i] == 't' || a[i] == 'T')
+			a[i] = '7';
+		if (a[i] == 'l' || a[i] == 'L')
+			a[i] = '1';
+	}
+}
