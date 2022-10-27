5bce5f68d3278ac1f2160e56647249b4d4ed3160
“chrizlm” <lmchris58@gmail.com>
2022-10-27 02:32:46 -0700

    concat string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - function
+ * concatinates strings
+ * @dest: destination
+ * @src: source string
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, n = 0, t = 0, countd = 0, counts = 0;
+
+	while (dest[n] != 0)
+	{
+		countd++;
+		n++;
+	}
+	while (src[t] != 0)
+	{
+		counts++;
+		t++;
+	}
+	for (i = 0, countd += 1 ;
+i <= counts ; i++)
+		dest[countd] = src[i];
+	dest[countd] = '\0';
+}
+
