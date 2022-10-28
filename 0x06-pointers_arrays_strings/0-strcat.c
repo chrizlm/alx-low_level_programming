a1228684e93d1e0f5960aebb1ee8da5e018ccd83
“chrizlm” <lmchris58@gmail.com>
2022-10-28 00:29:59 -0700

    changes to concat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,21 +10,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, n = 0, t = 0, countd = 0, counts = 0;
+	int n = 0, t = 0;
 
-	while (dest[n] != 0)
-	{
-		countd++;
+	while (dest[n] != '\0')
 		n++;
-	}
-	while (src[t] != 0)
+	while (src[t] != '\0')
 	{
-		counts++;
+		dest[n] = src[t];
+		n++;
 		t++;
 	}
-	for (i = 0, countd += 1;
-i <= counts ; i++)
-		dest[countd] = src[i];
-	dest[countd] = '\0';
+	dest[n] = '\0';
 	return (dest);
 }
