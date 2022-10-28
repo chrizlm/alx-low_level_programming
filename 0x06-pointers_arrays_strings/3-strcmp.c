c0ef379ac0d42724fdd64708de5f06c472d0666d
“chrizlm” <lmchris58@gmail.com>
2022-10-28 05:11:48 -0700

    string compare function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int n = 0, count1 = 0, count2 = 0, t = 0;
 
-	while (s1[n] != 0)
+	while (s1[n] != '\0')
 	{
-		count1 += count[n];
+		count1 += s1[n];
 		n++;
 	}
-	while (s2[t] != 0)
+	while (s2[t] != '\0')
 	{
-		count2 += count[t];
+		count2 += s2[t];
 		t++;
 	}
 	return (count1 - count2);
