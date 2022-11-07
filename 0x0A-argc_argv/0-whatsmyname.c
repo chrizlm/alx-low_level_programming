b1da770410e8eb14dbe7becfc5ce4294cd63d2be
chrizlm <lmchris58@gmail.com>
2022-11-07 23:55:04 +0300

    function name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-#define UNUSED(argc) (int)(argc)
+
 
 /**
-* main - function
-* @argc: count
-* @argv: vector
-* Return: 0
-*/
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
 
 int main(int argc, char* argv[])
 {
-	UNUSED(argc);
-
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
