e81e3b05a0513546925159e56fe7e10e7c54c8cd
chrizlm <lmchris58@gmail.com>
2022-11-07 23:45:33 +0300

    name of function

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
 #include "main.h"
+#define UNUSED(argc) (int)(argc)
 
+/**
+* main - function
+* @argc: count
+* @argv: vector
+* Return: 0
+*/
+
+int main(int argc, char* argv[])
+{
+	UNUSED(argc);
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
