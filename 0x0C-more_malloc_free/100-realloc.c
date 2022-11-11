c539cc46dfa2c5453a96b2eb420f50bddc85c867
chrizlm <lmchris58@gmail.com>
2022-11-11 22:38:48 +0300

    memory allocation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,19 +15,30 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-	if (new_size > old_size)
-		new_ptr = malloc(new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
 	if (new_size == old_size)
 		return (ptr);
 
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		else
+			return (new_ptr);
+	}
+
+	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
 
-	if (ptr == NULL)
-		return (NULL);
-
 	new_ptr = ptr;
+
 
 	return (new_ptr);
 }
