c0cbc5f890d726e852cff586c3b81fd45e1f21e8
chrizlm <lmchris58@gmail.com>
2022-11-12 13:04:03 +0300

    functions file

---
+++ 0x09-static_libraries/file3.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * _strcat - function
+ * @dest: input
+ * @src: input
+ * Return: char pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
+
+/**
+ * _strncat - function
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: char pointer
+ */
+
+char *_strncat(char *dest, char *src, int n __attribute__((unused)))
+{
+	dest = src;
+
+	return (dest);
+}
+
+/**
+ * _strncpy - function
+ * @dest: input
+ * @src: input
+ * @n: input
+ */
+
+char *_strncpy(char *dest, char *src, int n __attribute__((unused)))
+{
+	dest = src;
+
+	return (dest);
+}
+
+/**
+ * _strcmp - function
+ * @s1: input
+ * @s2: input
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	s1 = s2;
+
+	return (0);
+}
+
+/**
+ * _memset - function
+ * @s: input
+ * @b: input
+ * @n: input
+ * Return: char pointer
+ */
+
+char *_memset(char *s, char b __attribute__((unused)), unsigned int n __attribute__((unused)))
+{
+	return (s);
+}
