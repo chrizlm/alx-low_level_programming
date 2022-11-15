c2b54d57028f4002b45fcc59274dc2ba88ca88a0
chrizlm <lmchris58@gmail.com>
2022-11-15 23:22:23 +0300

    creating a new struct obj

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /**
+ * name_len -function
+ * @name: name input
+ * Return: char
+ */
+
+int name_len(char *name)
+{
+	int i;
+
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
  * new_dog - function
  * @name: name input
  * @age: age input
@@ -22,7 +37,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog_obj->age = age;
 	new_dog_obj->owner = owner;
 
+	name_cpy = malloc(name_len(name));
+	if (name_cpy == NULL)
+		return (NULL);
 	name_cpy = new_dog_obj->name;
+	owner_cpy = malloc(name_len(owner));
+	if (owner_cpy == NULL)
+		return (NULL);
 	owner_cpy = new_dog_obj->owner;
 
 	return (new_dog_obj);
