1b621185904fd9176f14fc22914e74e568839a1d
chrizlm <lmchris58@gmail.com>
2022-11-16 11:39:10 +0300

    file name macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+
+/**
+ * name_len -function
+ * @name: name input
+ * Return: char
+ */
+
+int name_len(char *name)
+{
+	int i;
+
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	return (i);
+}
 
 /**
  * main - function prints file name
@@ -8,6 +23,18 @@
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	int len, i;
+	char *name;
+
+	len = name_len(__FILE__);
+	name = malloc(len);
+	if (file_name == NULL)
+		return (1);
+
+	for (i = 0; name[i] != '\0'; i++)
+		_putchar('%c', name[i]);
+	_putchar('\0');
+	_putchar('\n');
+
 	return (0);
 }
