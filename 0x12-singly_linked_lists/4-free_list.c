775b5ed203c9f095bd1591c78b710ce0120e1b56
chrizlm <lmchris58@gmail.com>
2022-11-29 00:06:39 +0300

    free up a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,11 +8,13 @@
 
 void free_list(list_t *head)
 {
+	list_t *temp;
+
 	while (head != NULL)
 	{
-		free(head->str);
-		head->str = NULL;
-		free(head);
+		temp = head;
+		free(temp->str);
+		free(temp);
 		head = head->next;
 	}
 }
