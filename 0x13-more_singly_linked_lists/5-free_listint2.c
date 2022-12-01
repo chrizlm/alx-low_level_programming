574c6684d3c96ebc09482299fc4d0088ed6ea855
chrizlm <lmchris58@gmail.com>
2022-12-01 18:30:28 +0300

    free a linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		temp = *head;
+		*head = temp->next;
 		free(temp);
-		*head = head->next;
 	}
 
 	free(*head);
