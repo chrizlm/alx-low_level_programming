2a4427686ad049fd3d298ba5fa16317a408ac83d
chrizlm <lmchris58@gmail.com>
2022-12-01 18:29:33 +0300

    adding node at beginnig of a linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -20,14 +20,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (n == NULL);
-		n = 0;
-
 	new_node->n = n;
-	new_node->next = *head
+	new_node->next = *head;
 
 	*head = new_node;
-	free(new_node);
 
 	return (*head);
 }
